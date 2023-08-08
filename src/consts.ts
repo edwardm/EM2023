7f3a9146934eea7d42deb3ab0ba5e9dfd8ed8cea
Edward Margallo <etmargallo@gmail.com>
2023-08-08 14:27:16 -0700

    First major commit - changed blog collection to portfolio. Imported tailwindcss via npm package. Added a couple new pages.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Edward Margallo - Sr. UX Designer, Design Systems';
+export const SITE_DESCRIPTION = "I'm a senior and principle UI/UX Designer with experience in managing design teams. I build enterprise-level design systems from the ground up";
