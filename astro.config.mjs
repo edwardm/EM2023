a01b7397e5a5d4718fe6ea9f7b459df07f0f72fc
Edward Margallo <etmargallo@gmail.com>
2023-08-10 13:42:51 -0700

    Major style update
    Removed tailwindcss package, added astro bootstrap package. Focusing main styling on the home page components, colors, typography and spacing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import tailwind from "@astrojs/tailwind";
+// import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dev-astro.edwardmargallo.com/",
-  // base: '/em2023',
-  // root: './em2023',
-  //srcDir: './em2023',
-  // outDir: './em2023',
-  integrations: [mdx(), sitemap(), tailwind()]
+	site: "https://dev-astro.edwardmargallo.com/",
+	// base: '/em2023',
+	// root: './em2023',
+	//srcDir: './em2023',
+	// outDir: './em2023',
+	integrations: [mdx(), sitemap()],
 });
