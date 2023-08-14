4c37090502224b9200ce59624656427c272fad2f
Edward Margallo <etmargallo@gmail.com>
2023-08-14 15:44:09 -0700

    Template cleanup
    Adjusting the default template and styling

--- src/consts.ts
+++ src/consts.ts
@@ -5,4 +5,4 @@ export const SITE_TITLE = "Edward Margallo - Sr. UX Designer, Design Systems";
 export const MY_NAME = "Edward Margallo";
 export const MY_TITLE = "Sr. UX Designer, Design Systems";
 export const SITE_DESCRIPTION =
-	"I'm a senior and principle UI/UX Designer with experience in managing design teams. I build enterprise-level design systems from the ground up and also have exerpience in front-end development.";
+	"A senior UI/UX Designer with experience in managing design teams, building enterprise-level design systems from the ground-up and also works on front-end development.";
