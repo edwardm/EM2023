13280c5df5906a6e7d8695e083d1d18c625aa9c8
Edward Margallo <etmargallo@gmail.com>
2023-09-11 18:00:07 -0700

    Update consts.ts
    Title adjustment

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,6 @@
 
 export const SITE_TITLE = "Edward Margallo - Sr. UX Designer, Product Designer and Design Systems Lead";
 export const MY_NAME = "Edward Margallo";
-export const MY_TITLE = "Sr. UX Designer, Product Designer and Design Systems Lead";
+export const MY_TITLE = "Sr. UX & Product Designer, Design Systems";
 export const SITE_DESCRIPTION =
 	"A seasoned UI/UX Designer, who brings a wealth of expertise in leading design teams, architecting enterprise-level design systems from the ground up, and actively engaging in front-end development.";
