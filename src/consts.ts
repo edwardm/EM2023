8f229f3cb15d189d7ed47cee003792a1f0fa8598
Edward Margallo <etmargallo@gmail.com>
2023-09-20 11:57:03 -0700

    Consts vars update, Portfolio update
    Modified meta data and started new portfolio addition to the featured component

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Edward Margallo - Sr. UX Designer, Product Designer and Design Systems Lead";
+export const SITE_TITLE = "Edward Margallo - Lead UI/UX Designer and Front-End Developer and Design Systems Lead";
 export const MY_NAME = "Edward Margallo";
-export const MY_TITLE = "Sr. UX & Product Designer, Design Systems";
+export const MY_TITLE = "Lead UI/UX Designer & Design Systems";
 export const SITE_DESCRIPTION =
 	"A seasoned UI/UX Designer, who brings a wealth of expertise in leading design teams, architecting enterprise-level design systems from the ground up, and actively engaging in front-end development.";
